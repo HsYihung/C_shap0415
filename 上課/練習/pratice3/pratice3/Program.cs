95e1ff11a8de83ca6b40be50c23479f9afd3acfe
ccza <a125012239@gmail.com>
2022-04-14 09:32:09 +0800

    add pratice3

---
+++ 上課/練習/pratice3/pratice3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 便當選單 , 輸入便當名稱 , 告訴使用者有沒有販售 , 此便當多少錢
+            // 建立二維string陣列 , 儲存便當名稱跟價格 , 做搜尋
+            // 至少有十種便當 , string[20,2]
+
+            string[,] bangAtring =
+            {
+                {"雞腿便當","80元" },
+                {, },
+                {, },
+                {, },
+                {, },
+                {, },
+                {, },
+                {, },
+                {, },
+                {, }
+            }
+
+
+
+            Console.WriteLine("輸入任意鍵結束");
+            Console.ReadKey();
+        }
+    }
+}
