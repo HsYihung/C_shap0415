219a5aeb17f391f92b378e932c67baf9f8508dbb
ccza <a125012239@gmail.com>
2022-04-15 16:08:51 +0800

    2022.04.15

---
+++ 上課/作業/for01/for01/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //聖誕樹迴圈練習
+
+            string strInput = Console.ReadLine();
+            int hight_t = Convert.ToInt32(strInput);
+
+            if(hight_t <= 71 && hight_t>=23)
+            {
+                hight_t = hight_t-11;
+                int hight_t1 = hight_t/6  ;
+                int hight_t2 = hight_t % 4;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                for (int i =1;i<=11;i+=2)//樹梢繪製
+                {
+                    for(int j = 13; j>=i; j-=2)
+                    {
+                        Console.Write(" ");
+                    }
+                    for(int k = 1;k<=i;k++)
+                    {
+                        if(i==1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("*");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        }
+                        if(i==5 && k==1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        else if (i == 5 && k == 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        else if (i == 9 && (k==1|| k == 9))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        else if (i == 9 && k == 5 )
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                        }
+
+                        Console.Write("*");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.WriteLine();
+                }
+                for(int i = 1; i<=hight_t1;i++ )//樹葉繪製次數
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    for(int j = 5; j <=15;j+=2)//樹葉繪製
+                    {
+                        for(int k = 13; k>=j;k-=2)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for(int m = 1; m<=j; m++)
+                        {
+                            if(j==5)
+                            {
+                                Console.Write("*****");
+                                break;
+                            }
+
+
+                            if(j==7&&(m==1||m==j))
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                            }
+
+                            if(j==7&&m==j/2+1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                            }
+                            if(j == 11 && (m == 1 || m == j))
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                            }
+                            if(j==11 &&m==j / 2 - 1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                            }
+                            if (j == 11 && m == j / 2 + 3)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                            }
+                            if (j == 15 && m == j / 2 - 2)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                            }
+
+                            if (j == 15 && m == j / 2 +1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                            }
+
+                            if (j == 15 && m == j / 2 + 4)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                            }
+                            if (j == 15 && (m == 1 || m == j))
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                            }
+
+                            Console.Write("*");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                for(int i =1; i<=5+hight_t2; i++ )//樹幹繪製
+                {
+                    for(int j = 1; j<=5;j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for(int k = 1; k<=5;k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("高度值輸入錯誤");
+            }
+
+            Console.WriteLine("------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 上課/作業/pratice3/pratice3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 便當選單 , 輸入便當名稱 , 告訴使用者有沒有販售 , 此便當多少錢
+            // 建立二維string陣列 , 儲存便當名稱跟價格 , 做搜尋
+            // 至少有十種便當 , string[20,2]
+
+            string[,] bangAtring =
+            {
+                {"雞腿便當","80元" },
+                {"排骨便當","70元" },
+                {"滷雞腿便當","75元" },
+                {"叉燒便當","90元" },
+                {"炸排骨便當","85元" },
+                {"虱目魚肚便當","100元" },
+                {"宮保雞丁便當","75元" },
+                {"三杯雞便當","75元" },
+                {"香烤雞翅便當","80元" },
+                {"糖醋排骨便當","95元" }
+            };
+
+            while (true)
+            {
+                bool boolInput = true;
+                Console.Clear();
+                Console.WriteLine("請輸入想搜尋的便當名稱或輸入x退出 輸入f列出全部便當");
+                string strInput = Console.ReadLine();
+                if (strInput != "")
+                {
+                    if (strInput == "x")
+                    {
+                        break;
+                    }
+                    if (strInput == "f")
+                    {
+                        for(int i = 0; i <=9;i++)
+                        {
+                            Console.WriteLine(bangAtring[i,0] );
+                        }
+                        Console.WriteLine("請輸入便當名稱");
+                        strInput = Console.ReadLine();
+                    }
+                    for (int i = 0; i <= 9; i++)
+                    {
+                        if (bangAtring[i, 0] == strInput)
+                        {
+                            Console.WriteLine($"{bangAtring[i, 0]}  , {bangAtring[i, 1]}");
+                            Console.WriteLine("按下任意鍵繼續");
+                            Console.ReadKey();
+                            boolInput = false;
+                            break;
+                        }
+                    }
+                    if (boolInput)
+                    {
+                        Console.WriteLine("查無此便當");
+                        Console.WriteLine("按下任意鍵繼續");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("請重新輸入便當名稱");
+                    Console.WriteLine("按下任意鍵繼續");
+                    Console.ReadKey();
+                }
+            }
+
+            
+
+            Console.WriteLine("輸入任意鍵結束");
+            Console.ReadKey();
+        }
+    }
+}
