3663f97989c01a03652efb0574a7e25b5dc799d0
ccza <a125012239@gmail.com>
2022-04-29 16:05:34 +0800

    2022/04/29

---
+++ 上課/作業/bingo bingo/bingo bingo/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace bingo_bingo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBetNuber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBet = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLottoNumbers = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblBetNumber = new System.Windows.Forms.Label();
+            this.btnBet10 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSingle = new System.Windows.Forms.Button();
+            this.btnBig = new System.Windows.Forms.Button();
+            this.btnBet9 = new System.Windows.Forms.Button();
+            this.btnBet8 = new System.Windows.Forms.Button();
+            this.btnSmall = new System.Windows.Forms.Button();
+            this.btnEven = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.bntLottery = new System.Windows.Forms.Button();
+            this.lblWinning = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBetNuber
+            // 
+            this.txtBetNuber.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtBetNuber.Location = new System.Drawing.Point(126, 204);
+            this.txtBetNuber.Name = "txtBetNuber";
+            this.txtBetNuber.Size = new System.Drawing.Size(54, 27);
+            this.txtBetNuber.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(108, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bingo Bingo";
+            // 
+            // btnBet
+            // 
+            this.btnBet.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBet.Location = new System.Drawing.Point(186, 204);
+            this.btnBet.Name = "btnBet";
+            this.btnBet.Size = new System.Drawing.Size(53, 27);
+            this.btnBet.TabIndex = 2;
+            this.btnBet.Text = "確認";
+            this.btnBet.UseVisualStyleBackColor = true;
+            this.btnBet.Click += new System.EventHandler(this.btnBet_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(137, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "開獎號碼";
+            // 
+            // lblLottoNumbers
+            // 
+            this.lblLottoNumbers.BackColor = System.Drawing.Color.White;
+            this.lblLottoNumbers.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblLottoNumbers.Location = new System.Drawing.Point(46, 90);
+            this.lblLottoNumbers.Name = "lblLottoNumbers";
+            this.lblLottoNumbers.Size = new System.Drawing.Size(275, 51);
+            this.lblLottoNumbers.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(137, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "投注號碼";
+            // 
+            // lblBetNumber
+            // 
+            this.lblBetNumber.BackColor = System.Drawing.Color.White;
+            this.lblBetNumber.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblBetNumber.Location = new System.Drawing.Point(46, 174);
+            this.lblBetNumber.Name = "lblBetNumber";
+            this.lblBetNumber.Size = new System.Drawing.Size(275, 27);
+            this.lblBetNumber.TabIndex = 6;
+            // 
+            // btnBet10
+            // 
+            this.btnBet10.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBet10.Location = new System.Drawing.Point(38, 443);
+            this.btnBet10.Name = "btnBet10";
+            this.btnBet10.Size = new System.Drawing.Size(53, 27);
+            this.btnBet10.TabIndex = 7;
+            this.btnBet10.Text = "十星";
+            this.btnBet10.UseVisualStyleBackColor = true;
+            this.btnBet10.Click += new System.EventHandler(this.btnBet10_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(137, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "玩法選擇";
+            // 
+            // btnSingle
+            // 
+            this.btnSingle.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSingle.Location = new System.Drawing.Point(150, 443);
+            this.btnSingle.Name = "btnSingle";
+            this.btnSingle.Size = new System.Drawing.Size(53, 27);
+            this.btnSingle.TabIndex = 9;
+            this.btnSingle.Text = "單";
+            this.btnSingle.UseVisualStyleBackColor = true;
+            this.btnSingle.Click += new System.EventHandler(this.btnSingle_Click);
+            // 
+            // btnBig
+            // 
+            this.btnBig.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBig.Location = new System.Drawing.Point(264, 443);
+            this.btnBig.Name = "btnBig";
+            this.btnBig.Size = new System.Drawing.Size(53, 27);
+            this.btnBig.TabIndex = 10;
+            this.btnBig.Text = "大";
+            this.btnBig.UseVisualStyleBackColor = true;
+            this.btnBig.Click += new System.EventHandler(this.btnBig_Click);
+            // 
+            // btnBet9
+            // 
+            this.btnBet9.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBet9.Location = new System.Drawing.Point(38, 476);
+            this.btnBet9.Name = "btnBet9";
+            this.btnBet9.Size = new System.Drawing.Size(53, 27);
+            this.btnBet9.TabIndex = 11;
+            this.btnBet9.Text = "九星";
+            this.btnBet9.UseVisualStyleBackColor = true;
+            this.btnBet9.Click += new System.EventHandler(this.btnBet9_Click);
+            // 
+            // btnBet8
+            // 
+            this.btnBet8.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBet8.Location = new System.Drawing.Point(38, 509);
+            this.btnBet8.Name = "btnBet8";
+            this.btnBet8.Size = new System.Drawing.Size(53, 27);
+            this.btnBet8.TabIndex = 12;
+            this.btnBet8.Text = "八星";
+            this.btnBet8.UseVisualStyleBackColor = true;
+            this.btnBet8.Click += new System.EventHandler(this.btnBet8_Click);
+            // 
+            // btnSmall
+            // 
+            this.btnSmall.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSmall.Location = new System.Drawing.Point(264, 509);
+            this.btnSmall.Name = "btnSmall";
+            this.btnSmall.Size = new System.Drawing.Size(53, 27);
+            this.btnSmall.TabIndex = 13;
+            this.btnSmall.Text = "小";
+            this.btnSmall.UseVisualStyleBackColor = true;
+            this.btnSmall.Click += new System.EventHandler(this.btnSmall_Click);
+            // 
+            // btnEven
+            // 
+            this.btnEven.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnEven.Location = new System.Drawing.Point(150, 509);
+            this.btnEven.Name = "btnEven";
+            this.btnEven.Size = new System.Drawing.Size(53, 27);
+            this.btnEven.TabIndex = 14;
+            this.btnEven.Text = "雙";
+            this.btnEven.UseVisualStyleBackColor = true;
+            this.btnEven.Click += new System.EventHandler(this.btnEven_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(21, 416);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 24);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "基本玩法";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(146, 416);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 24);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "猜單雙";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(260, 416);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 24);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "比大小";
+            // 
+            // bntLottery
+            // 
+            this.bntLottery.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bntLottery.Location = new System.Drawing.Point(105, 598);
+            this.bntLottery.Name = "bntLottery";
+            this.bntLottery.Size = new System.Drawing.Size(134, 42);
+            this.bntLottery.TabIndex = 18;
+            this.bntLottery.Text = "開獎";
+            this.bntLottery.UseVisualStyleBackColor = true;
+            this.bntLottery.Click += new System.EventHandler(this.bntLottery_Click);
+            // 
+            // lblWinning
+            // 
+            this.lblWinning.BackColor = System.Drawing.Color.White;
+            this.lblWinning.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblWinning.Location = new System.Drawing.Point(46, 258);
+            this.lblWinning.Name = "lblWinning";
+            this.lblWinning.Size = new System.Drawing.Size(275, 124);
+            this.lblWinning.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(137, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 24);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "中獎訊息";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(358, 642);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblWinning);
+            this.Controls.Add(this.bntLottery);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnEven);
+            this.Controls.Add(this.btnSmall);
+            this.Controls.Add(this.btnBet8);
+            this.Controls.Add(this.btnBet9);
+            this.Controls.Add(this.btnBig);
+            this.Controls.Add(this.btnSingle);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnBet10);
+            this.Controls.Add(this.lblBetNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblLottoNumbers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBetNuber);
+            this.Name = "Form1";
+            this.Text = "Bingo Bingo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBetNuber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLottoNumbers;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblBetNumber;
+        private System.Windows.Forms.Button btnBet10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSingle;
+        private System.Windows.Forms.Button btnBig;
+        private System.Windows.Forms.Button btnBet9;
+        private System.Windows.Forms.Button btnBet8;
+        private System.Windows.Forms.Button btnSmall;
+        private System.Windows.Forms.Button btnEven;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button bntLottery;
+        private System.Windows.Forms.Label lblWinning;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 上課/作業/bingo bingo/bingo bingo/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bingo_bingo
+{
+    public partial class Form1 : Form
+    {
+        int lotteType = 0;
+        int lotteTypeStar = 0;
+        int lotteLength = 0;
+        int[] betArray;
+        int[] lotteArray;
+        int x = 0;
+        int supLotteNumber = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lotteArray = new int[20];
+            betArray = new int[10];
+        }
+
+        private void btnBet_Click(object sender, EventArgs e)
+        {
+
+
+            if (lotteType >= 8 && x < lotteType && lotteType <= 10)
+            {
+                if (betConfirm(txtBetNuber.Text))
+                {
+                    betArray[x] = Convert.ToInt32(txtBetNuber.Text);
+                    lblBetNumber.Text += $"{txtBetNuber.Text.PadLeft(2, '0')} ";
+                    x += 1;
+                }
+            }
+            else if(lotteType > 100)
+            {
+                MessageBox.Show("超過投注上限");
+            }
+            else if (lotteType > 10 )
+            {
+                MessageBox.Show("所選擇玩法不需要投注號碼");
+            }
+            else if (lotteType == 0)
+            {
+                MessageBox.Show("尚未選擇玩法");
+            }           
+            if(x == lotteType )
+            {
+                lotteType += 100;
+            }
+            txtBetNuber.Clear();
+        }
+
+        private void btnBet10_Click(object sender, EventArgs e)
+        {
+            lotteType = 10;
+            Array.Clear(betArray,0,10);
+            lblBetNumber.Text = "";
+        }
+
+        private void btnBet9_Click(object sender, EventArgs e)
+        {
+            lotteType = 9;
+            Array.Clear(betArray, 0, 10);
+            lblBetNumber.Text = "";
+        }
+
+        private void btnBet8_Click(object sender, EventArgs e)
+        {
+            lotteType = 8;
+            Array.Clear(betArray, 0, 10);
+            lblBetNumber.Text = "";
+        }
+
+        private void btnSingle_Click(object sender, EventArgs e)
+        {
+            lotteType = 11;
+            Array.Clear(betArray, 0, 10);
+            lblBetNumber.Text = "";
+        }
+
+        private void btnEven_Click(object sender, EventArgs e)
+        {
+            lotteType = 12;
+            Array.Clear(betArray, 0, 10);
+            lblBetNumber.Text = "";
+        }
+
+        private void btnBig_Click(object sender, EventArgs e)
+        {
+            lotteType = 13;
+            Array.Clear(betArray, 0, 10);
+            lblBetNumber.Text = "";
+        }
+
+        private void btnSmall_Click(object sender, EventArgs e)
+        {
+            lotteType = 14;
+            Array.Clear(betArray, 0, 10);
+            lblBetNumber.Text = "";
+            lblLottoNumbers.Text = "";
+        }
+
+        private void bntLottery_Click(object sender, EventArgs e)
+        {
+            if (lotteType <= 10)
+            {
+                if (lotteType == 0)
+                {
+                    MessageBox.Show("請選擇玩法");
+                }
+                else
+                {
+                    MessageBox.Show("投注號碼數目不正確");
+                }
+            }
+            else
+            {
+                testLotte();
+                switch (lotteType)
+                {
+                    case 11:
+
+                        break;
+                    case 12:
+
+                        break;
+                    case 13:
+
+                        break;
+                    case 14:
+
+                        break;
+                    case 108:
+                        int winNumber = winStar();
+                        if(winNumber>=4)
+                        {
+
+                        }
+                        else
+                        {
+                            lblWinning.Text += "\n沒有中獎";
+                        }
+                        
+                        break;
+                    case 109:
+
+                        break;
+                    case 110:
+
+                        break;
+                }
+            }
+        }
+       
+        //myMethods
+        bool betConfirm(string a)
+        {
+            bool betConfirm = true;
+            try
+            {
+                int b = Convert.ToInt32(a);
+                if (b>0 && b<=80)
+                {
+                    if (betArray.Length > 0)
+                    {
+                        for (int i = 0; i < betArray.Length; i++)
+                        {
+                            if (betArray[i] == b)
+                            {
+                                betConfirm = false;
+                                MessageBox.Show("重複輸入請重新輸入");
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    betConfirm = false;
+                    MessageBox.Show("請1~80整數數字");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("請輸入數字");
+                betConfirm = false;
+            }
+            return betConfirm;
+        }
+        void testLotte()
+        {
+            int[] testLotteArray = new int[20] { 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 14, 67, 63, 65, 2, 4, 6, 8, 10 };
+            Array.Copy(testLotteArray, lotteArray, testLotteArray.Length);
+            supLotteNumber = lotteArray[lotteArray.GetUpperBound(0)];
+            Array.Sort(lotteArray);
+            for(int i = 0; i<lotteArray.Length;i++)
+            {
+                lblLottoNumbers.Text += string.Format($"{lotteArray[i]:D2} ");
+            }            
+        }
+        int winStar()
+        {
+            int myWinning = 0;
+            string winNumber = "";
+            for (int i = 0; i < lotteArray.Length; i++)
+            {
+                if (Array.IndexOf(betArray, lotteArray[i]) > -1)
+                {
+                    winNumber += string.Format($"{lotteArray[i]:D2} ");
+                    myWinning += 1;
+                }
+            }
+            lblWinning.Text =string.Format($"超級號號:{supLotteNumber:D2}\n中獎號碼:{winNumber}\n中了{myWinning}個號碼") ;
+            return myWinning;
+        }
+    }
+}
